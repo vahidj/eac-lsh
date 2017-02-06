4a68345a2757cb841c916a7b9f0e6bf6548c6a9d
v0j0017 <vjalali@walmartlabs.com>
2017-02-05 22:13:42 -0800

    added println to debug

--- tank_run.sh
+++ tank_run.sh
@@ -1,3 +1,3 @@
-#mvn clean package -P tank
+mvn clean package -P tank
 #bash $SPARK_HOME/bin/spark-submit --class org.soic.eaclsh.KNNTester --master local[1] --conf "spark.executor.extraJavaOptions=-XX:+UseG1GC -XX:ConcGCThread=1 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" --driver-memory 5g ./target/eaclsh-0.1-jar-with-dependencies.jar ./
 bash $SPARK_HOME/bin/spark-submit --class org.soic.eaclsh.KNNTester --master local[2] ./target/eaclsh-0.1-jar-with-dependencies.jar ./
